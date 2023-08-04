dc6131ee06e5a80b8a56c623715621a764274857
Alexey Gribov <ultraangarsk@gmail.com>
2023-08-04 11:03:21 +0700

    04.08.23

--- blocks/popup/__close/popup__close.js
+++ blocks/popup/__close/popup__close.js
@@ -7,11 +7,12 @@ buttonClose.forEach((item) => {
 
   //навешиваем на текущий элемент слушатель
   item.addEventListener('click', function (event) {
-    event.target.parentElement.stilee
+  //  event.target.parentElement.classList.add('popup_close1');
+
     event.target.parentElement.classList.remove('popup_opened');
 
 
-    console.log(event.target.parentElement);
+
   });
 
 });
