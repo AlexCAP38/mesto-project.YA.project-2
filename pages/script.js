de037710289c776c71032df5f42b15a53608b2be
Alexey Gribov <ultraangarsk@gmail.com>
2023-08-23 22:12:38 +0700

    Реализовать события по нажатию клавиатуры и мыши

--- pages/script.js
+++ pages/script.js
@@ -56,8 +56,8 @@ const popupNewCard = document.querySelector('#popup-new-card'); //находим
 
 profileAddButton.addEventListener('click', function () {  //Открытие попапа при нажатие на кнопку
 
-    openPopup(popupNewCard); //открываем попап
-    popupNewCard.querySelector('#popup__form-new-card').reset(); //очищаем форму
+  openPopup(popupNewCard); //открываем попап
+  popupNewCard.querySelector('#popup__form-new-card').reset(); //очищаем форму
 
 });
 
@@ -192,13 +192,39 @@ function showImage(url, name) {
   //присваиваем название изображения
   popupViewerTitle.textContent = name;
   openPopup(popupViewerContainer); //открываем попап "просомтр фотографии"
+
 };
 
 //Функция открытия попапа
+//when it is popap opening add event keydown
 //===========================================================================================
 
 function openPopup(popup) {
   popup.classList.add('popup_opened'); //открываем попап
+
+  //Устанавливаем слушатель на весь документ. отслеживаем нажатие клавиш.
+  document.addEventListener('keydown', (event) => {
+
+    //Если нажата клавиша Escape, вызовим функция закрытия попапа
+    if (event.key === 'Escape') {
+      closePopup(popup);
+    }
+
+  });
+
+  //Устанавливаем слушатель на клик мыши на всю форму модального окна
+  popup.addEventListener('click', (event) => {
+
+    //Оверлей это наш весь попап если по нему щелнули тогда вызываем функцию закрывия окна
+    if (popup === event.target) {
+      closePopup(popup);
+    }
+
+  });
+
+
+
+
 }
 
 //Функция закрытия попапа попапа
@@ -206,4 +232,11 @@ function openPopup(popup) {
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+
+  //чтобы прослушивания события не занимала память удаляем ее
+  document.removeEventListener('keydown', (event) => { });
+  popup.removeEventListener('click', (event) => { });
+
+
+
 }
